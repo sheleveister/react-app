9a36e67bf58408514b0cc94e32a48d04ea0e2761
sheleveister <e.sheleveister@gmail.com>
2018-03-18 20:32:09 +0300

    add props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { PropTypes, Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
+class App extends Component {
+
+  static propTypes = {
+    btnText: PropTypes.string.isRequired,
+    h1Text: PropTypes.string.isRequired,
+    newArray: PropTypes.array.isRequired
+  };
+
+  static defaultProps = {
+    btnText: 'Default text'
+  };
+
+  btnOnClick(event) {
+    return event.target;
+  }
+
   render() {
     return (
-      <h1>App works!</h1>
+      <div className="test">
+        <h1>{ this.props.h1Text }</h1>
+        <h3>It is really working</h3>
+        <button onClick={this.btnOnClick}>{ this.props.btnText }</button>
+      </div>
     );
   }
 }
 
 ReactDOM.render(
-  <App/>, document.getElementById('app')
+  <App btnText="Click btn" h1Text="This is h1 text" newArray={[1, 2, 3]}/>,
+  document.getElementById('app')
 );
